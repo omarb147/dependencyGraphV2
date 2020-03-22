a0f0bdb20a72dbf299a91c05362d00fa0bfe224f
Omar Bello <omar.bello@clientrelationship.com>
2020-03-22 10:46:58 +0000

    updated test

--- src/components/Card/tests/Card.test.tsx
+++ src/components/Card/tests/Card.test.tsx
@@ -5,7 +5,7 @@ import Card from '../Card';
 describe('<Card/>', () => {
   it('should render a card with color and text', () => {
     const size = { width: 100, height: 200 };
-    const { getByText } = render(<Card text="Hello World" color="yellow" size={size} />);
-    expect(getByText('Hello World')).toMatchSnapshot();
+    const { container } = render(<Card text="Hello World" color="yellow" size={size} />);
+    expect(container).toMatchSnapshot();
   });
 });
