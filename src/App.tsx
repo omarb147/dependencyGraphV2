818f3bea7716f4b820102a6b2cf9c16e3f79fd61
Omar Bello <omar.bello@clientrelationship.com>
2020-03-22 11:26:25 +0000

    added relative importing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './Redux/Store';
+import store from '@src/Redux/Store';
 import { Graph } from './components';
 
 export default () => (
