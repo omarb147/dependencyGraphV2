2e017521d022a59e84edc44bef3d4716021161eb
Alex Wong <alex.wong@clientrelationship.com>
2020-03-26 07:48:56 +0000

    updated the styling of the card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -8,17 +8,62 @@ interface ICardProps {
   className?: string;
 }
 
-const Card: React.SFC<ICardProps> = ({ className, text }: ICardProps) => (
-  <div className={className}>
-    <div>{text}</div>
-  </div>
+const CardFrame = styled.div`
+  background-color:#FFFFFF;
+  border-top: 5px solid ${(props) => props.color};
+  width:20rem;
+  box-shadow: 0px 5px 10px #C9C9C9;
+  padding: 1rem;
+`;
+
+const UserStoryWrapper = styled.div`
+  color: #434343;
+  font-size: 1rem;
+  display:flex;
+  justify-content: space-between;
+`;
+
+const Point = styled.div`
+  padding: 0.25rem 0.4rem;
+  font-size: 1.2rem;
+  background-color: ${(props) => props.color};
+  color: #FFFFFF;
+  margin-right: 0.5rem;
+  opacity:0.7;
+`;
+
+const Divider = styled.hr`
+  margin: 1rem 0 0.5rem;
+  border-top:1px solid #E1E1E1;
+`;
+
+const Text = styled.div`
+  flex-grow: 1;
+`;
+
+const LabelFrame = styled.div`
+display:flex;
+justify-content: flex-end;
+color: #A5A5A5;
+font-size:0.8rem;
+`;
+
+
+const Card: React.SFC<ICardProps> = ({ className, text, color }: ICardProps) => (
+  <CardFrame color={color} className={className}>
+    <UserStoryWrapper>
+      <Point color={color}>
+        1
+      </Point>
+      <Text>
+        {text}
+      </Text>
+    </UserStoryWrapper>
+    <Divider />
+    <LabelFrame>
+      #LastSprint
+    </LabelFrame>
+  </CardFrame>
 );
 
-export default styled(Card)`
-  background-color: ${(props): string => props.color};
-  width: 100%;
-  height: 100%;
-  div {
-    font-size: 1.5em;
-  }
-`;
+export default Card;
