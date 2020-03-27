dfc31d12919a2868d23baa87e13719cb111809bf
Omar Bello <omar.bello@clientrelationship.com>
2020-03-27 21:35:55 +0000

    added keypress event listner and deselectable background

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -1,27 +1,59 @@
 import React from 'react';
-import { INodesState } from '@/type/types';
+import styled from 'styled-components';
+import { INode } from '@/type/types';
 import NodesDispatchClass from '@/module/node/dispatch';
 import NodesSelectorClass from '@/module/node/selectors';
 import Node from '@/components/Node';
+import { useKeyPress } from '../../hooks/useKeyPress';
 
-const generateNodes = (nodes: INodesState) => Object.entries(nodes).map(([id, node]) => {
+const GraphOuter = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const generateNodes = (
+  nodes: { [index: string]: INode },
+  selectedNodes: string[],
+) => Object.entries(nodes).map(([id, node]) => {
   const { text, color } = node;
-  return <Node key={id} id={id} text={text} color={color} />;
+  const selected = selectedNodes.includes(id) === true;
+  return (
+    <Node key={id} id={id} text={text} color={color} selected={selected} />
+  );
 });
 
-const Graph: React.SFC = () => {
+const Graph: React.FC = () => {
   const NodesDispatch = new NodesDispatchClass();
   const NodesSelector = new NodesSelectorClass();
 
   const nodes = NodesSelector.useNodes();
+  const selectedNodes = NodesSelector.useSelectedNodes();
+
+  const keyDown = useKeyPress('Enter');
+
+  /* eslint-disable */
+  keyDown && console.log('enter');
 
   return (
-    <div>
-      {generateNodes(nodes)}
-      <button type="button" onClick={() => NodesDispatch.addNode('test', 'green')}>
-        Test
-      </button>
-    </div>
+    /* eslint-disable */
+    <>
+      <div>
+        {generateNodes(nodes, selectedNodes)}
+        <button
+          type="button"
+          onClick={() => NodesDispatch.addNode('test', 'green')}
+        >
+          Test
+        </button>
+      </div>
+      <GraphOuter
+        onClick={(event) => {
+          event.stopPropagation();
+          NodesDispatch.deselectAllNodes();
+        }}
+      ></GraphOuter>
+      s
+    </>
   );
 };
 export default Graph;
