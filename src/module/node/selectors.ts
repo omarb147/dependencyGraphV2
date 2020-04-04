de98e3abf0232cda93297679ce835c421b0057f1
Alex Wong <alex.wong@clientrelationship.com>
2020-04-04 19:03:57 +0100

    added selector to get headers and added return types

--- src/module/node/selectors.ts
+++ src/module/node/selectors.ts
@@ -1,14 +1,18 @@
 import SelectorClass from '@/module/util/rootSelcector';
-import { IFullNode, INodesState, INode } from '@/type/types';
+import {
+  IFullNode, INodesState, INode, IFullHeader, INodeSize, INodeCoordinates,
+} from '@/type/types';
 
 export default class NodeSelector extends SelectorClass {
   useNodeState = (): INodesState => this.useSelector((state) => state.node)
 
   useNodes = (): IFullNode => this.useNodeState().nodes;
 
+  useHeaders = (): IFullHeader => this.useNodeState().headers;
+
   useNodeById = (id: string): INode => this.useNodes()[id]
 
-  useNodePositionAndSize = (id: string) => this.useSelector((state) => {
+  useNodePositionAndSize = (id: string): {position: INodeCoordinates; size: INodeSize} => this.useSelector((state) => {
     const { position, size } = state.node.nodes[id];
     return { position, size };
   });
